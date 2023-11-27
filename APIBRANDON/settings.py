0512508e512570bac79da821ca191507db058600
brandon180601 <brandon.silva.180@gmail.com>
2023-11-27 15:24:25 -0600

    static

--- APIBRANDON/settings.py
+++ APIBRANDON/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-*s&1pg(*4c7+h%v4)31&c@6*$nsuzc7$kw-(m50*byzws=z3y2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
